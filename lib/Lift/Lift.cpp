f763702fcc514c98c02b04f3e366a136544d64ed
Anton Johansson <anjo@rev.ng>
2022-08-08 23:00:36 +0200

    [WIP] Update `Lift` to `libtcg`
    Signed-off-by: Anton Johansson <anjo@rev.ng>

--- lib/Lift/Lift.cpp
+++ lib/Lift/Lift.cpp
@@ -45,42 +45,42 @@ findExternalFilePaths(const model::Architecture::Values Architecture) {
 
   const std::string ArchName = model::Architecture::getQEMUName(Architecture).str();
 
-  ExternalFilePaths paths = {};
+  ExternalFilePaths Paths = {};
 
   const std::string LibTcgName = "/lib/libtcg-" + ArchName + ".so";
   auto OptionalLibTcg = ResourceFinder.findFile(LibTcgName);
   revng_assert(OptionalLibTcg.has_value(), "Cannot find libtinycode");
-  paths.LibTcg = OptionalLibTcg.value();
+  Paths.LibTcg = OptionalLibTcg.value();
 
   const std::string LibHelpersName = "/lib/libtcg-helpers-" + ArchName + ".bc";
   auto OptionalHelpers = ResourceFinder.findFile(LibHelpersName);
   revng_assert(OptionalHelpers.has_value(), "Cannot find tinycode helpers");
-  paths.LibHelpers = OptionalHelpers.value();
+  Paths.LibHelpers = OptionalHelpers.value();
 
   const std::string EarlyLinkedName = "/share/revng/early-linked-" + ArchName + ".ll";
   auto OptionalEarlyLinked = ResourceFinder.findFile(EarlyLinkedName);
   revng_assert(OptionalEarlyLinked.has_value(), "Cannot find early-linked.ll");
-  paths.EarlyLinked = OptionalEarlyLinked.value();
+  Paths.EarlyLinked = OptionalEarlyLinked.value();
 
-  return paths;
+  return Paths;
 }
 
 bool LiftPass::runOnModule(llvm::Module &M) {
   const auto &ModelWrapper = getAnalysis<LoadModelWrapperPass>().get();
   const TupleTree<model::Binary> &Model = ModelWrapper.getReadOnlyModel();
 
-  const auto paths = findExternalFilePaths(Model->Architecture);
+  const auto Paths = findExternalFilePaths(Model->Architecture);
 
   // Look for the library in the system's paths
-  void *LibraryHandle = dlopen(paths.LibTcg.c_str(), RTLD_LAZY | RTLD_NODELETE);
+  void *LibraryHandle = dlopen(Paths.LibTcg.c_str(), RTLD_LAZY | RTLD_NODELETE);
   if (LibraryHandle == nullptr) {
     fprintf(stderr, "Couldn't load the libtcg library: %s\n", dlerror());
     return EXIT_FAILURE;
   }
 
   // Obtain the address of the libtcg_load entry point
-  using LibTcgLoadFunc = LIBTCG_FUNC_TYPE(libtcg_load);
-  auto LibTcgLoad = reinterpret_cast<LibTcgLoadFunc *>(dlsym(LibraryHandle, "libtcg_load"));
+  //using LibTcgLoadFunc = LIBTCG_FUNC_TYPE(libtcg_load);
+  auto LibTcgLoad = reinterpret_cast<LIBTCG_FUNC_TYPE(libtcg_load) *>(dlsym(LibraryHandle, "libtcg_load"));
   if (LibTcgLoad == nullptr) {
     fprintf(stderr, "Couldn't find symbol libtcg_load: %s\n", dlerror());
     return EXIT_FAILURE;
@@ -92,17 +92,17 @@ bool LiftPass::runOnModule(llvm::Module &M) {
   // Get access to raw binary data
   RawBinaryView &RawBinary = getAnalysis<LoadBinaryWrapperPass>().get();
 
-  //CodeGenerator Generator(RawBinary,
-  //                        &M,
-  //                        Model,
-  //                        paths.LibHelpers,
-  //                        paths.EarlyLinked,
-  //                        model::Architecture::x86_64);
+  CodeGenerator Generator(RawBinary,
+                          &M,
+                          Model,
+                          Paths.LibHelpers,
+                          Paths.EarlyLinked,
+                          model::Architecture::x86_64);
 
   llvm::Optional<uint64_t> EntryPointAddressOptional;
   if (EntryPointAddress.getNumOccurrences() != 0)
     EntryPointAddressOptional = EntryPointAddress;
-  //Generator.translate(LibTcg, EntryPointAddressOptional);
+  Generator.translate(LibTcg, EntryPointAddressOptional);
 
   dlclose(LibraryHandle);
 
